4c1dc0b32c9467b433dee5a35881bbbb32a6571f
Thiago Guerrero <tgbalera@hotmail.com>
2022-06-25 17:44:37 -0300

    add confidence interval generator
    Signed-off-by: Thiago Guerrero <tgbalera@hotmail.com>

---
+++ measurement_scripts/statsGenerator.js
@@ -0,0 +1,28 @@
+import { argv, exit } from 'node:process';
+import * as fs from 'fs';
+
+const MEASUREMENTS = 1000;
+
+if(argv.length < 2) {
+  console.log("[ERROR] You must pass the file path!");
+  exit(1);
+}
+
+const filepath = argv[2];
+fs.readFile(filepath, 'utf8', (err, data) => {
+  if(err) throw err;
+  
+  const dataArray = data.split('\n');
+  dataArray.pop(); //Empty line
+  let timeUsage = dataArray.reduce((acc, measure) => Number(measure) + acc, 0);
+  timeUsage /= MEASUREMENTS;
+  
+	// variance estimator: s = sqrt(1 / (n - 1) * sum_1^n(X[i] - mean)^2)
+  let varianceEstimator = dataArray.reduce((acc, measure) => acc + Math.pow(Number(measure) - timeUsage, 2), 0);
+  varianceEstimator = Math.sqrt(varianceEstimator);
+  varianceEstimator /= Math.sqrt(MEASUREMENTS);
+
+  const left = timeUsage - 1.96*varianceEstimator;
+  const right = timeUsage + 1.96*varianceEstimator;
+  console.log(`[${left} | ${right} ]`);
+});
